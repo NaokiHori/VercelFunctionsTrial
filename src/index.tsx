dac879b965146703df7424590765fb431277d753
NaokiHori <36466440+NaokiHori@users.noreply.github.com>
2024-07-01 21:31:28 +0200

    [add] 設定と最小限のファイル群

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { createRoot, Root } from "react-dom/client";
+
+function App(): JSX.Element {
+  return <></>;
+}
+
+const id = "root";
+const rootElement: HTMLElement | null = document.getElementById(id);
+if (rootElement === null) {
+  throw new Error(`The root element "${id}" is not found`);
+}
+const root: Root = createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
